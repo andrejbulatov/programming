2cf004d0f0c238237c5600586445e84ffe893f05
Andrei Bulatov <andbulatov@mail.ru>
2023-02-10 18:38:16 +0300

    Add Example008_dog_and_friends

---
+++ Example008_dog_and_friends/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+double distance = 20000;
+double firstFriendSpeed = 1;
+double secondFriendSpeed = 2;
+double dogSpeed = 5;
+int friend = 2;
+double time;
+
+while (distance > 10)
+{
+  if (friend == 1)
+  {
+    time = distance / (firstFriendSpeed + dogSpeed);
+    friend = 2;
+  }
+  else
+  {
+    time = distance / (secondFriendSpeed + dogSpeed);
+    friend = 1;
+  }
+  distance = distance - ((firstFriendSpeed + secondFriendSpeed) * time);
+  count = count + 1;
+}
+
+Console.WriteLine(count);
